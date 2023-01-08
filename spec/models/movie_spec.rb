033603815ab305cfbf9346a7bd0e09a99bf3522b
Patrick Espake <patrick@envixo.com>
2023-01-08 18:57:46 +0000

    rspec tests for movie

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Movie, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    describe 'with subject' do
+      subject { build(:movie) }
+      
+      it { is_expected.to validate_presence_of(:name) }
+      it { is_expected.to validate_presence_of(:thumbnail) }
+      it { is_expected.to validate_presence_of(:image) }
+      it { is_expected.to validate_presence_of(:market_price) }
+      it { is_expected.to validate_uniqueness_of(:name).scoped_to(:owner_id) }
+    end
+  end
+
+  describe 'associations' do
+    it { is_expected.to belong_to(:owner) }
+    it { is_expected.to have_and_belong_to_many(:genres) }
+  end
+
+  describe 'callbacks' do
+    it 'sets the parameterized name before saving' do
+      movie = build(:movie)
+      movie.save
+      expect(movie.parameterized_name).to eq(movie.name.parameterize)
+    end
+  end
 end
