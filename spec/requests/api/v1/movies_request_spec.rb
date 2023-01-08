a870eb3d869c4edd231743ca5a28b47000081405
Patrick Espake <patrick@envixo.com>
2023-01-08 21:53:33 +0000

    fixed rspec tests

--- spec/requests/api/v1/movies_request_spec.rb
+++ spec/requests/api/v1/movies_request_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe "Movies", type: :request do
       get api_v1_movies_path
 
       expect(response).to have_http_status(200)
-      expect(response.body).to eq(Movie.all.to_json)
+      expect(response.body).to eq(Movie.all.to_json(include: [:owner, :genres]))
     end
 
     context "when searching" do
@@ -18,7 +18,7 @@ RSpec.describe "Movies", type: :request do
         get api_v1_movies_path, params: { q: { name_cont: "matrix" } }
 
         expect(response).to have_http_status(200)
-        expect(response.body).to eq([movie].to_json)
+        expect(response.body).to eq([movie].to_json(include: [:owner, :genres]))
       end
     end
 
@@ -27,14 +27,14 @@ RSpec.describe "Movies", type: :request do
         get api_v1_movies_path, params: { page: 1, per_page: 1 }
 
         expect(response).to have_http_status(200)
-        expect(response.body).to eq([movies[0]].to_json)
+        expect(response.body).to eq([movies[0]].to_json(include: [:owner, :genres]))
       end
 
       it "returns the second page" do
         get api_v1_movies_path, params: { page: 2, per_page: 2 }
 
         expect(response).to have_http_status(200)
-        expect(response.body).to eq([movies[2]].to_json)
+        expect(response.body).to eq([movies[2]].to_json(include: [:owner, :genres]))
       end
     end
   end
@@ -44,7 +44,7 @@ RSpec.describe "Movies", type: :request do
       movie = create(:movie)
       get api_v1_movie_path(movie)
       expect(response).to have_http_status(200)
-      expect(response.body).to eq(movie.to_json)
+      expect(response.body).to eq(movie.to_json(include: [:owner, :genres]))
     end
   end
 end
